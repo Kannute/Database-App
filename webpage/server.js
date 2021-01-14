2b11f1c4087d6eb7051c3cb12637fa44e5e9db93
Kannute <kacper.jan.luka@gmail.com>
2021-01-14 18:41:58 +0100

    updated

--- webpage/server.js
+++ webpage/server.js
@@ -1,5 +1,6 @@
 const {Pool} = require("pg")
-const express = require ("express")
+const express = require ("express");
+//const { connect } = require("http2");
 const app = express();
 app.use(express.json());
 const pool = new Pool({
@@ -10,3 +11,34 @@ const pool = new Pool({
 });
 
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
+
+app.get("/wszyscy_wiezniowie", async(req,res)=> {
+    const rows = await readAllPrisoners();
+    res.setHeader("content-type", "application/json");
+    res.send(JSON.stringify(rows))
+})
+
+start()
+
+async function start(){
+    await connect();
+}
+
+async function connect(){
+    try{
+        await pool.connect();
+    }
+    catch(e){
+        console.log("Failed to connect");
+    }
+}
+
+async function readAllPrisoners(){
+    try{
+        const results = await pool.query("select * from wszyscy wiezniowie");
+        return results.rows;
+    }
+    catch(e){
+        return [];
+    }
+}
