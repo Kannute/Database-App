795f92abace5f4ff22f3bebdec77202832e6a98f
Kannute <kacper.jan.luka@gmail.com>
2021-01-14 20:58:36 +0100

    added nodejs

--- webpage/server.js
+++ webpage/server.js
@@ -1,7 +1,8 @@
 const {Pool} = require("pg")
 const express = require ("express");
-//const { connect } = require("http2");
 const app = express();
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 const pool = new Pool({
     user: "rrmazoiy",
@@ -10,7 +11,9 @@ const pool = new Pool({
     password: "7UbgjJtkYd5IZqPZlBAIenrECU1rbHL0" 
 });
 
+
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
+app.get("/raporty.html", (req, res) => res.sendFile(`${__dirname}/raporty.html`))
 
 app.get("/wszyscy_wiezniowie", async(req,res)=> {
     const rows = await readAllPrisoners();
@@ -18,6 +21,8 @@ app.get("/wszyscy_wiezniowie", async(req,res)=> {
     res.send(JSON.stringify(rows))
 })
 
+
+app.listen(8080, () => console.log("Web server is listening.. on port 8080"))
 start()
 
 async function start(){
@@ -35,7 +40,8 @@ async function connect(){
 
 async function readAllPrisoners(){
     try{
-        const results = await pool.query("select * from wszyscy wiezniowie");
+        const results = await pool.query("select * from wszyscy_wiezniowie;");
+        //console.table(JSON.stringify(results.rows));
         return results.rows;
     }
     catch(e){
